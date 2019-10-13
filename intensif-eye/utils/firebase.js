6dfd25325c2784d2940b8a4772ae8c2f1e84b844
Giulio Rossi <ciuffi9@mac.com>
2019-10-12 18:23:45 -0700

    finish up firebase upload

--- intensif-eye/utils/firebase.js
+++ intensif-eye/utils/firebase.js
@@ -1,14 +1,5 @@
 import firebase from 'firebase'
-import RNFetchBlob from 'react-native-fetch-blob'
 import uuid from 'react-native-uuid';
-
-// Initialize blob
-const Blob = RNFetchBlob.polyfill.Blob
-const fs = RNFetchBlob.fs
-window.XMLHttpRequest = RNFetchBlob.polyfill.XMLHttpRequest
-window.Blob = Blob
-
-
 // Initialize Firebase
 const firebaseConfig = {
     apiKey: "AIzaSyDV0-438GM9g_aYHf08qv3ldwj8JRmiv14",
@@ -24,24 +15,19 @@ const firebaseConfig = {
 const firebaseApp = firebase.initializeApp(firebaseConfig)
 
 export default firebaseApp
-const mime = 'application/octet-stream'
+const mime = 'image/jpg'
 
 
-export const uploadImage = async ((uri) => {
+export const uploadImage = async (imageURI) => {
     try {
-    const uploadUri = Platform.OS === 'ios' ? uri.replace('file://', '') : uri
-    let uploadBlob = null
-
-    const imageRef = firebaseApp.storage().ref('images').child(uuid.v1())
-
-    const base64Data = await fs.readFile(uploadUri, 'base64')
-    const builtBlob = await Blob.build(base64Data, { type: `${mime};BASE64` })
-    const uploadBlob = builtBlob
-    await imageRef.put(blob, { contentType: mime })
-    uploadBlob.close()
-    return imageRef.getDownloadURL()
+        const response = await fetch(imageURI);
+        const blob = await response.blob();
+        const imageRef = firebaseApp.storage().ref(`${uuid.v1()}.jpg`)
+        await imageRef.put(blob, { contentType: mime })
+        const uri = await imageRef.getDownloadURL()
+        console.log(uri)
     }catch(e) {
         console.log(e)
         throw(e)
     }
-})
+}
