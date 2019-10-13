2511908ed4a39a189c7fe03e873dd35c3766699c
Allison Chiang <acchiang98@gmail.com>
2019-10-13 00:49:06 -0700

    update home screen button layout

--- intensif-eye/screens/HomeScreen.js
+++ intensif-eye/screens/HomeScreen.js
@@ -71,28 +71,50 @@ export default class HomeScreen extends Component {
           <View style={styles.welcomeContainer}>
             <Image
               source={
-                __DEV__
-                  ? require("../assets/images/robot-dev.png")
-                  : require("../assets/images/robot-prod.png")
+                require("../assets/images/small-eye.png")
               }
               style={styles.welcomeImage}
             />
           </View>
+          <View style={styles.tabBarInfoContainer}>
+            <TouchableOpacity onPress = {() => this.initiatePhotoQuery(CAMERA)}>
+              <Image
+                  source={
+                    require("../assets/images/camera.png")
+                  }
+                  style={styles.buttonImageLeft}
+                />
+            </TouchableOpacity>
+            <TouchableOpacity onPress = {() => this.initiatePhotoQuery(GALLERY)}>
+            <Image
+                source={
+                  require("../assets/images/upload.png")
+                }
+                style={styles.buttonImageRight}
+              />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.tabBarInfoContainer}>
+            <TouchableOpacity onPress = {() => {/* todo */}}>
+              <Image
+                  source={
+                    require("../assets/images/saved.png")
+                  }
+                  style={styles.buttonImageLeft}
+                />
+            </TouchableOpacity>
+            <TouchableOpacity onPress = {() => {/* todo */}}>
+            <Image
+                source={
+                  require("../assets/images/setup.png")
+                }
+                style={styles.buttonImageRight}
+              />
+            </TouchableOpacity>
+          </View>
         </ScrollView>
 
-        <View style={styles.tabBarInfoContainer}>
-          <Button
-            title="Choose From Gallery"
-            onPress={() => this.initiatePhotoQuery(GALLERY)}
-          ></Button>
-          <Button
-            title="Take Photo"
-            onPress={() => this.initiatePhotoQuery(CAMERA)}
-          ></Button>
-          <View
-            style={[styles.codeHighlightContainer, styles.navigationFilename]}
-          ></View>
-        </View>
+        
       </View>
     );
   }
@@ -140,7 +162,7 @@ function handleHelpPress() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff"
+    backgroundColor: "#070F3B"
   },
   developmentModeText: {
     marginBottom: 20,
@@ -158,8 +180,7 @@ const styles = StyleSheet.create({
     marginBottom: 20
   },
   welcomeImage: {
-    width: 100,
-    height: 80,
+    width: 150,
     resizeMode: "contain",
     marginTop: 3,
     marginLeft: -10
@@ -186,24 +207,32 @@ const styles = StyleSheet.create({
     textAlign: "center"
   },
   tabBarInfoContainer: {
-    position: "absolute",
     bottom: 0,
     left: 0,
     right: 0,
     ...Platform.select({
-      ios: {
-        shadowColor: "black",
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3
-      },
       android: {
         elevation: 20
       }
     }),
     alignItems: "center",
-    backgroundColor: "#fbfbfb",
-    paddingVertical: 20
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  buttonImageLeft: {
+    width: 150,
+    height: 150,
+    resizeMode: "contain",
+    margin: 10,
+    marginLeft: 50
+  },
+  buttonImageRight: {
+    width: 150,
+    height: 150,
+    resizeMode: "contain",
+    margin: 10,
+    marginRight: 50
   },
   tabBarInfoText: {
     fontSize: 17,
@@ -224,11 +253,5 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: "#2e78b7"
   },
-  cameraButton: {
-    fontSize: 14,
-    color: "#f5793a",
-    textAlign: "center",
-    marginTop: 20,
-    width: 50
-  }
+
 });
